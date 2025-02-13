0003c3cf42abeba525d0d3f01d22c7312aedddda
Mathis <wer.mathis@gmail.com>
2025-02-13 15:20:18 +0100

    First test of readout of all four sensors

--- PCA95_test.py
+++ PCA95_test.py
@@ -2,7 +2,6 @@
 Tca9548a basic usage example
 """
 import time
-
 import tca9548a
 import smbus2
 
@@ -25,16 +24,25 @@ def example():
     # disable channel 4
     #tca_driver.set_channel(2, 0)
 # init
-#i2c_address = 0x70
-#tca_driver = tca9548a.TCA9548A(i2c_address)
+# I2C Adresses of Multiplexer and Sensors
+PCA95_ADDR = 0x71
+HCL_ADDR = 0x78
+# Initialise I2C Bus
 bus = smbus2.SMBus(1)
+# Initialise Multiplexer
+tca_driver = tca9548a.TCA9548A(bus, PCA95_ADDR)
+# Disable all I2C channels on multiplexer
+tca_driver.set_control_register(0b00000000)
 time.sleep(3)
-example()
 
 while True:
-    time.sleep(3)
-    data = bus.read_byte(0x78)
-    data <<= 8
-    result = ((float(data) - 1638) / 5253) - 2.47
-    print(result)
-    time.sleep(3)
+    for i in range(3):
+        # Enable channel
+        tca_driver.set_channel(i, 1)
+        data = bus.read_byte(HCL_ADDR)
+        data <<= 8
+        result = ((float(data) - 1638) / 5253) - 2.47
+        print(f"Measured pressure at sensor {i} is {result:.2f} mbar")
+        # Disable channel 4
+        tca_driver.set_channel(i, 0)
+        time.sleep(1)
