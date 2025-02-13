f24deee6931ba9182d7850eecfcc87ea23df94ad
Mathis <wer.mathis@gmail.com>
2025-02-13 17:23:39 +0100

    Added main file, created HCL object file and cleaned up

---
+++ hcla02x5eb.py
@@ -0,0 +1,17 @@
+import time
+import tca9548a
+import smbus2
+
+class HCLA02X5EB():
+    def __init__(self, i2c_bus, address):
+        """Init HCLA02X5EB sensor constants"""
+        self.P_min = -2.5     # Minimum pressure readout
+        self.Out_min = 1638   # Minimum count readout
+        self.S = (27852 - 1638)/(2.5 - self.P_min)
+        self.i2c_address = address
+        self.i2c_bus = i2c_bus
+    def get_pressure_reading(self):
+        data = self.i2c_bus.read_byte(self.i2c_address)
+        data <<= 8
+        result = ((float(data) - self.Out_min) / self.S) - self.P_min
+        return result
---
+++ main.py
@@ -0,0 +1,27 @@
+import hcla02x5eb
+import tca9548a
+import smbus2
+import time
+
+# Constants
+HCLA02X5EB_ADDR = 0x78
+TCA9548A_ADDR = 0x71
+
+# Setup I2C port 1
+bus = smbus2.SMBus(1)
+# Setup pressure sensor
+HCL_sens = hcla02x5eb.HCLA02X5EB(bus, HCLA02X5EB_ADDR)
+# Setup Multiplexer
+TCA_multiplex = tca9548a.TCA9548A(bus, TCA9548A_ADDR)
+# Disable all I2C channels on multiplexer
+TCA_multiplex.set_control_register(0b00000000)
+
+while True:
+    for i in range(4):
+        # Enable channel
+        TCA_multiplex.set_channel(i, 1)
+        pressure = HCL_sens.get_pressure_reading()
+        print(f"Measured pressure at sensor {i+1} is {pressure:.2f} mbar")
+        # Disable channel
+        TCA_multiplex.set_channel(i, 0)
+    time.sleep(1)
