659cf02f9698f099930848dce69fc0b08f2c9ae7
Mathis <wer.mathis@gmail.com>
2025-02-27 12:44:14 +0100

    Minor cleanup in main

--- main.py
+++ main.py
@@ -12,21 +12,24 @@ logger = Logger()
 # Setup drone object for telemetry
 drone = UAVTracker()
 
+# Initialise and start threads
 sensorReadoutThread = threading.Thread(target=sensorReadout(logger, bus), daemon=True)
 updateDroneThread = threading.Thread(target=drone.run(), daemon=True)
 
 sensorReadoutThread.start()
 updateDroneThread.start()
 
-# Setup dummy variable to detect True false transition in logging switch
+# Setup dummy variable to detect True False transition in logging switch
 previous_logging_state = False
 
+# Main loop
 while True:
     if not previous_logging_state and drone.logging_enabled:
         logger.create_csv
-        previous_logging_state = drone.logging_enabled
     if drone.logging_enabled:
         logger.log_data("Latitude", drone.latitude)
         logger.log_data("Longitude", drone.longitude)
         logger.write_data_to_csv()
+        
+    previous_logging_state = drone.logging_enabled
     time.sleep(logger.sample_time)
