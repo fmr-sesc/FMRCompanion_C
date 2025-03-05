ca71b8f53e227e0cefbfcfb6b2a9603b2df14a89
Mathis <wer.mathis@gmail.com>
2025-03-05 13:24:32 +0100

    added dummy_enable flag to create and run log on startup

--- main.py
+++ main.py
@@ -23,13 +23,14 @@ updateDroneThread.start()
 # Setup dummy variable to detect True False transition in logging switch
 previous_logging_state = False
 
+dummy_enable = True
 # Main loop (mainly used for logging and to keep threads running)
 while True:
     # If logging switch changes from True to False create new csv
-    if not previous_logging_state and drone.logging_enabled:
+    if not previous_logging_state and dummy_enable:
         logger.create_csv
     # Write collected data to csv (sensor data already loaded to buffer)
-    if drone.logging_enabled:
+    if dummy_enable:
         logger.log_data("Latitude", drone.latitude)
         logger.log_data("Longitude", drone.longitude)
         logger.write_data_to_csv()
