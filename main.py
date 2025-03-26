ec8c4cb3ceda0d4d140ab90efbe372f51969205d
Mathis <wer.mathis@gmail.com>
2025-03-26 17:10:14 +0100

    Enabled logging on arm

--- main.py
+++ main.py
@@ -28,17 +28,16 @@ updateDroneThread.start()
 # Setup dummy variable to detect True False transition in logging switch
 previous_logging_state = False
 
-dummy_enable = True
 # Main loop (mainly used for logging and to keep threads running)
 while True:
     # If logging switch changes from True to False create new csv
-    if not previous_logging_state and dummy_enable:
+    if not previous_logging_state and drone.logging_enabled:
         logger.create_csv()
     # Write collected data to csv (sensor data already loaded to buffer)
-    if dummy_enable:
+    if drone.logging_enabled:
         logger.log_data("Latitude", drone.latitude)
         logger.log_data("Longitude", drone.longitude)
         logger.write_data_to_csv()
         
-    previous_logging_state = dummy_enable
+    previous_logging_state = drone.logging_enabled
     time.sleep(logger.sample_time)
