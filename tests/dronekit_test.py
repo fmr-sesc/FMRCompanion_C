de3754e3d2e2b4da222bee77608cf78a3f182c62
Mathis <wer.mathis@gmail.com>
2025-04-04 12:58:56 +0200

    Debug

--- tests/dronekit_test.py
+++ tests/dronekit_test.py
@@ -2,7 +2,7 @@ from dronekit import connect
 import time
 
 print("Connecting to drone")
-vehicle = connect('udp:192.168.0.4:14540', wait_ready=True)
+vehicle = connect('udp:0.0.0.0:14540', wait_ready=True)
 print("Drone Connected!")
 while True:
     print(vehicle.battery)
