a0e468a1e6781b134747ed4b73ffaeb1a5d3d1ac
Mathis <wer.mathis@gmail.com>
2025-04-04 12:49:43 +0200

    Added dronekit test script

---
+++ tests/dronekit_test.py
@@ -0,0 +1,9 @@
+from dronekit import connect
+import time
+
+print("Connecting to drone")
+vehicle = connect('192.168.0.4:14540', wait_ready=True)
+print("Drone Connected!")
+while True:
+    print(vehicle.battery)
+    time.sleep(1)
