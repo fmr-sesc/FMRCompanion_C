ac417cf1f4251f6b5f8448cda356cf7c4f6b1403
Mathis <wer.mathis@gmail.com>
2025-04-15 13:50:24 +0200

    Debug

--- dronekit_test.py
+++ dronekit_test.py
@@ -8,6 +8,7 @@ updateDroneThread = threading.Thread(target=drone.run, daemon=True)
 updateDroneThread.start()
 
 while True:
-    print(UAVTracker_DK.latitude)
-    print(UAVTracker_DK.longitude)
-    print(UAVTracker_DK.logging_enabled)
+    #print(UAVTracker_DK.latitude)
+    #print(UAVTracker_DK.longitude)
+    #print(UAVTracker_DK.logging_enabled)
+    time.sleep(0.001)
