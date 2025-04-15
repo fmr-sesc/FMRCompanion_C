87c793953070b58ff45ae7639ef8b641fc5bf22d
Mathis <wer.mathis@gmail.com>
2025-04-15 14:03:16 +0200

    Debug

--- dronekit_test.py
+++ dronekit_test.py
@@ -1,31 +1,6 @@
+from tools import UAVTracker_DK
 import time
 import threading
-from dronekit import connect
-
-class UAVTracker_DK:
-    """Class to track the UAV's latitude and longitude in real-time."""
-    def __init__(self, drone_address='udpout:192.168.0.4:14540', usb_path = None, sample_time = 0.1):
-        self.latitude = 0.0
-        self.longitude = 0.0
-        self.drone_address = drone_address
-        self.logging_enabled = False
-        self.usb_path = usb_path
-        self.sample_time = sample_time
-
-    def run(self):
-        time.sleep(1)
-        print("Starting UDP connection to Pixhawk")
-        vehicle = connect(self.drone_address)
-        print("Pixhawk connected")
-
-        while True:
-            self.latitude = vehicle.location.global_frame.lat
-            self.longitude = vehicle.location.global_frame.lon
-            self.logging_enabled = vehicle.armed
-            print(self.latitude)
-            print(self.longitude)
-            print(self.logging_enabled)
-            time.sleep(self.sample_time)
 
 drone = UAVTracker_DK()
 
@@ -33,6 +8,6 @@ updateDroneThread = threading.Thread(target=drone.run, daemon=True)
 updateDroneThread.start()
 
 while True:
-    print(UAVTracker_DK.latitude)
-    print(UAVTracker_DK.longitude)
-    print(UAVTracker_DK.logging_enabled)
+    print(drone.latitude)
+    print(drone.longitude)
+    print(drone.logging_enabled)
