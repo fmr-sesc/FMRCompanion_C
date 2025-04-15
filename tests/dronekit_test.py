e371a6ae9047c8705fc724cec97d24ad87c3b60c
Mathis <wer.mathis@gmail.com>
2025-04-15 11:40:05 +0200

    Adjusted dronekit_test

--- tests/dronekit_test.py
+++ tests/dronekit_test.py
@@ -1,7 +1,7 @@
 from dronekit import connect
 
 # Connect to the Vehicle (in this case a UDP endpoint)
-vehicle = connect('192.168.0.4:14540', wait_ready=True)
+vehicle = connect('0.0.0.0:14540', wait_ready=True)
 
 # Wait for the first heartbeat
 #   This sets the system and component ID of remote system for the link
