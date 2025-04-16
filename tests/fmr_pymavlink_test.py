1454efcf60ef8b2a82ed704669b74ad4294365ce
Mathis <wer.mathis@gmail.com>
2025-04-16 14:41:54 +0200

    Filled fmr_pymavlink_test

--- tests/fmr_pymavlink_test.py
+++ tests/fmr_pymavlink_test.py
@@ -1,1 +1,5 @@
 from pymavlink.dialects.v10 import common as mavlink1
+from pymavlink import mavutil
+
+# Start a connection listening on a UDP port
+the_connection = mavutil.mavlink_connection('udpout:192.168.0.4:14540', dialect="common")
