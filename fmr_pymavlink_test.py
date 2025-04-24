d2567a01452d8fc462c35a5eb293a02081a89f63
Mathis <wer.mathis@gmail.com>
2025-04-24 13:13:07 +0200

    Debug

--- fmr_pymavlink_test.py
+++ fmr_pymavlink_test.py
@@ -5,7 +5,7 @@ from pymavlink.dialects.v10 import common as mavlink1
 from pymavlink import mavutil
 
 # Start a connection listening on a UDP port
-the_connection = mavutil.mavlink_connection('udpout:192.168.0.4:14540', dialect="common")
+the_connection = mavutil.mavlink_connection('udpout:192.168.0.4:14540', dialect="pymavlink.dialects.v10.common")
 
 the_connection.wait_heartbeat()
 print("Heartbeat from system (system %u component %u)" % (the_connection.target_system, the_connection.target_component))
