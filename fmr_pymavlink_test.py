f2848ac2b08b3e76da9b02e215742db8cb83c1bf
Mathis <wer.mathis@gmail.com>
2025-04-24 14:06:43 +0200

    pymavlink test now example from outdated doc

--- fmr_pymavlink_test.py
+++ fmr_pymavlink_test.py
@@ -1,18 +1,54 @@
-import os
+"""
+Example of how to connect to the autopilot by using mavproxy's
+--udpin:0.0.0.0:9000 endpoint from the companion computer itself
+"""
 
-os.environ['MAVLINK_DIALECT'] = 'common'
+# Disable "Bare exception" warning
+# pylint: disable=W0702
 
+import time
+# Import mavutil
 from pymavlink import mavutil
 
-# Start a connection listening on a UDP port
-the_connection = mavutil.mavlink_connection('udpout:192.168.0.4:14540', dialect="common")
+def wait_conn():
+    """
+    Sends a ping to stabilish the UDP communication and awaits for a response
+    """
+    msg = None
+    while not msg:
+        master.mav.ping_send(
+            int(time.time() * 1e6), # Unix time in microseconds
+            0, # Ping number
+            0, # Request ping of all systems
+            0 # Request ping of all components
+        )
+        msg = master.recv_match()
+        time.sleep(0.5)
 
-try:
-    altitude = the_connection.messages['GPS_RAW_INT'].alt  # Note, you can access message fields as attributes!
-    timestamp = the_connection.time_since('GPS_RAW_INT')
-    print(timestamp)
-except:
-    print('No GPS_RAW_INT message received')
+# Create the connection
+#  Companion is already configured to allow script connections under the port 9000
+# Note: The connection is done with 'udpout' and not 'udpin'.
+#  You can check in http:192.168.1.2:2770/mavproxy that the communication made for 9000
+#  uses a 'udp' (server) and not 'udpout' (client).
+master = mavutil.mavlink_connection('udpout:192.168.0.4:14540')
 
-the_connection.wait_heartbeat()
-print("Heartbeat from system (system %u component %u)" % (the_connection.target_system, the_connection.target_component))
+# Send a ping to start connection and wait for any reply.
+#  This function is necessary when using 'udpout',
+#  as described before, 'udpout' connects to 'udpin',
+#  and needs to send something to allow 'udpin' to start
+#  sending data.
+wait_conn()
+
+# Get some information !
+while True:
+    try:
+        print(master.recv_match().to_dict())
+    except:
+        pass
+    time.sleep(0.1)
+
+# Output:
+# {'mavpackettype': 'AHRS2', 'roll': -0.11364290863275528, 'pitch': -0.02841472253203392, 'yaw': 2.0993032455444336, 'altitude': 0.0, 'lat': 0, 'lng': 0}
+# {'mavpackettype': 'AHRS3', 'roll': 0.025831475853919983, 'pitch': 0.006112074479460716, 'yaw': 2.1514968872070312, 'altitude': 0.0, 'lat': 0, 'lng': 0, 'v1': 0.0, 'v2': 0.0, 'v3': 0.0, 'v4': 0.0}
+# {'mavpackettype': 'VFR_HUD', 'airspeed': 0.0, 'groundspeed': 0.0, 'heading': 123, 'throttle': 0, 'alt': 3.129999876022339, 'climb': 3.2699999809265137}
+# {'mavpackettype':
