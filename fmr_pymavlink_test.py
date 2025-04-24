520228b0fb1723efa1284879f16c69c40e783f0b
Mathis <wer.mathis@gmail.com>
2025-04-24 13:25:24 +0200

    Debug

--- fmr_pymavlink_test.py
+++ fmr_pymavlink_test.py
@@ -1,11 +1,14 @@
-import sys
-sys.path.append("/home/FMRCompanion/FMR-mavlink/pymavlink")
-
-from pymavlink.dialects.v10 import common as mavlink1
 from pymavlink import mavutil
 
 # Start a connection listening on a UDP port
 the_connection = mavutil.mavlink_connection('udpout:192.168.0.4:14540', dialect="pymavlink.dialects.v10.common")
+
+try:
+    altitude = the_connection.messages['GPS_RAW_INT'].alt  # Note, you can access message fields as attributes!
+    timestamp = the_connection.time_since('GPS_RAW_INT')
+    print(timestamp)
+except:
+    print('No GPS_RAW_INT message received')
 
 the_connection.wait_heartbeat()
 print("Heartbeat from system (system %u component %u)" % (the_connection.target_system, the_connection.target_component))
