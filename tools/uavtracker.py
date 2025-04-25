a31676ab68083306940a154f34cfd442639dc210
Mathis <wer.mathis@gmail.com>
2025-04-25 12:12:18 +0200

    Debug

--- tools/uavtracker.py
+++ tools/uavtracker.py
@@ -30,6 +30,33 @@ class UAVTracker:
         self.wait_conn()
         print("Drone Connected")
 
+        # Define command_long_encode message to send MAV_CMD_SET_MESSAGE_INTERVAL command
+        # param1: MAVLINK_MSG_ID_BATTERY_STATUS (message to stream)
+        # param2: 1000000 (Stream interval in microseconds)
+        message = self.vehicle.mav.command_long_encode(
+                self.vehicle.target_system,  # Target system ID
+                self.vehicle.target_component,  # Target component ID
+                mavutil.mavlink.MAV_CMD_SET_MESSAGE_INTERVAL,  # ID of command to send
+                0,  # Confirmation
+                mavutil.mavlink.MAVLINK_MSG_ID_GPS_RAW_INT,  # param1: Message ID to be streamed
+                10000, # param2: Interval in microseconds
+                0,       # param3 (unused)
+                0,       # param4 (unused)
+                0,       # param5 (unused)
+                0,       # param5 (unused)
+                0        # param6 (unused)
+                )
+
+        # Send the COMMAND_LONG
+        self.vehicle.mav.send(message)
+
+        # Wait for a response (blocking) to the MAV_CMD_SET_MESSAGE_INTERVAL command and print result
+        response = self.vehicle.recv_match(type='COMMAND_ACK', blocking=True)
+        if response and response.command == mavutil.mavlink.MAV_CMD_SET_MESSAGE_INTERVAL and response.result == mavutil.mavlink.MAV_RESULT_ACCEPTED:
+            print("Command accepted")
+        else:
+            print("Command failed")
+
 
         while True:
                 try:
