bccda5641708607fd8ea826f1c02b478e108951a
Sebastian Schubert <sebastian.schubert@tu-dresden.de>
2025-10-06 17:15:00 +0200

    changed I2C-Addr from 0x71 to 0x72 & pressure values from 4 to 3

--- threads/sensorReadout.py
+++ threads/sensorReadout.py
@@ -1,25 +1,25 @@
 from peripherals import HCLA02X5EB, TCA9548A
 import time
 
-def sensorReadout(logger, bus, drone, sample_time=0.01, HCLA02X5EB_ADDR=0x78, TCA9548A_ADDR=0x71):
+def sensorReadout(logger, bus, drone, sample_time=0.01, HCLA02X5EB_ADDR=0x78, TCA9548A_ADDR=0x72):
     '''Function to readout all sensor data and automatically write the data to the logger buffer'''
     # Setup pressure sensor
     HCL_sens = HCLA02X5EB(bus, HCLA02X5EB_ADDR)
     # Setup Multiplexer
-    TCA_multiplex = TCA9548A(bus, TCA9548A_ADDR)
+    TCA_multiplex = TCA9548A(bus, TCA9548A_ADDR=0x72)
     # Disable all I2C channels on multiplexer
     TCA_multiplex.set_control_register(0b00000000)
 
     # Main Loop
     while True:
         # Readout and log Pressure sensors
-        for i in range(4):
+        for i in range(3):
             # Enable channel
             TCA_multiplex.set_channel(i, 1)
             pressure = HCL_sens.get_pressure_reading()
             logger.log_data(f"Pressure Sensor {i+1}", pressure)
             drone.mav_sensor_values[i] = pressure
-            #print(f"Written pressure at sensor {i+1} is {drone.mav_sensor_values[i]:.2f} mbar")
+            print(f"Written pressure at sensor {i+1} is {drone.mav_sensor_values[i]:.2f} mbar")
             # Disable channel
             TCA_multiplex.set_channel(i, 0)
         time.sleep(sample_time)
